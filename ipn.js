b670b89139c622aee322259bae2bd155a62a6895
gokturkenez <hi@gokturkenez.com.tr>
2018-02-21 10:23:50 +0300

    Clear unnecessary code blogs

--- ipn.js
+++ ipn.js
@@ -41,6 +41,4 @@ var server = app.listen(port, function () {
     var host = server.address().address
     var port = server.address().port
 
-    console.log('Example app listening at http://%s:%s', host, port)
-
 });
